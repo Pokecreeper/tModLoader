9c7790fff506139f913375d32dd04f4014a06b57
InvalidOS <35383068+Pokecreeper@users.noreply.github.com>
2019-11-05 18:12:50 -0500

    Fix typo in comment
    "Wired" is an entirely different word than "weird" is.

--- ExampleMod/Items/Weapons/ExampleSpear.cs
+++ ExampleMod/Items/Weapons/ExampleSpear.cs
@@ -27,7 +27,7 @@ namespace ExampleMod.Items.Weapons
 
 			item.melee = true;
 			item.noMelee = true; // Important because the spear is actually a projectile instead of an item. This prevents the melee hitbox of this item.
-			item.noUseGraphic = true; // Important, it's kind of wired if people see two spears at one time. This prevents the melee animation of this item.
+			item.noUseGraphic = true; // Important, it's kind of weird if people see two spears at one time. This prevents the melee animation of this item.
 			item.autoReuse = true; // Most spears don't autoReuse, but it's possible when used in conjunction with CanUseItem()
 
 			item.UseSound = SoundID.Item1;
